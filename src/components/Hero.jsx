0364b85d1cf8801f5cb328a57bc398a4f0d5fdce
Abdullah <bh532997@gmail.com>
2024-04-03 04:00:20 +0300

    try deploy s

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,8 @@ import Section from "./Section";
 import Button from "./Button";
 import { BackgroundCircles, Gradient, BottomLine } from "./design/Hero";
 import { heroIcons } from "../constants";
+import { Generating } from "./Generating";
+import { Notification } from "./Notification";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 
@@ -22,7 +24,7 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilites of AI Chatting with{" "}
+            Explore the Possibilites of &npsb;AI&npsb; Chatting with{" "}
             <span className="inline-block relative">
               Brainwave{" "}
               <img
@@ -57,6 +59,8 @@ const Hero = () => {
                   alt="hero"
                 />
 
+                <Generating className={"absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2"}/>
+
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
                     {heroIcons.map((icon, index) => (
@@ -66,8 +70,14 @@ const Hero = () => {
                     ))}
                   </ul>
                 </ScrollParallax>
+
+                <ScrollParallax isAbsolutelyPositioned>
+                      <Notification className={"hidden"} />
+                </ScrollParallax>
+
               </div>
             </div>
+            <Gradient />
           </div>
           <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
@@ -78,8 +88,10 @@ const Hero = () => {
               alt="hero"
             />
           </div>
+          <BackgroundCircles />
         </div>
       </div>
+      <BottomLine />
     </Section>
   );
 };
